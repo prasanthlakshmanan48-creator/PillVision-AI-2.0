c00a90fae0ee5a93e71237dc4588a9b519e83716
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-08-01 14:16:42 +0530

    Update ocr.py

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,1 +1,18 @@
+import easyocr
+import numpy as np
 
+# Load OCR model once
+reader = easyocr.Reader(['en'], gpu=False)
+
+def extract_text(image):
+    """
+    Extract text from uploaded PIL image.
+    """
+
+    img = np.array(image)
+
+    results = reader.readtext(img)
+
+    text = " ".join([item[1] for item in results])
+
+    return text
