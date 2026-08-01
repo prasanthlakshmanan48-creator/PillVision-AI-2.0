55acd8c0731ee02c7b3ca57a0a13420512ed71c9
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-08-01 13:24:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from google import genai
 
-MODEL_NAME = "gemini-2.5-flash"
+MODEL_NAME = "gemini-flash-latest"
 
 client = genai.Client(
     api_key=st.secrets["GEMINI_API_KEY"]
