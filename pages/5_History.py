af0fe8c7a82f5a391a6002fa3e6d9b15d83293e7
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-08-01 13:47:25 +0530

    Implement history management functions
    Added functions to manage history in Streamlit app.

--- pages/5_History.py
+++ pages/5_History.py
@@ -1,1 +1,33 @@
+import streamlit as st
+from datetime import datetime
 
+
+def add_history(history_type, title, content):
+
+    if "history" not in st.session_state:
+        st.session_state.history = []
+
+    st.session_state.history.insert(0, {
+        "type": history_type,
+        "title": title,
+        "content": content,
+        "time": datetime.now().strftime("%d-%m-%Y %H:%M")
+    })
+
+
+def get_history():
+
+    if "history" not in st.session_state:
+        st.session_state.history = []
+
+    return st.session_state.history
+
+
+def clear_history():
+
+    st.session_state.history = []
+
+
+def history_count():
+
+    return len(get_history())
