32a987ae5cf9e87aad2797b8bfb8d2e218e3d2ab
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-08-01 14:10:21 +0530

    Update pdf.py

--- utils/pdf.py
+++ utils/pdf.py
@@ -1,1 +1,20 @@
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
 
+styles = getSampleStyleSheet()
+
+def create_pdf(title, content, filename):
+
+    pdf = SimpleDocTemplate(filename)
+
+    story = []
+
+    story.append(Paragraph(f"<b>{title}</b>", styles["Title"]))
+
+    story.append(Paragraph("<br/>", styles["Normal"]))
+
+    story.append(Paragraph(content.replace("\n", "<br/>"), styles["BodyText"]))
+
+    pdf.build(story)
+
+    return filename
