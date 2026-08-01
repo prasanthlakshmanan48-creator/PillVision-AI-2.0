4e14edefbd0c705c795b0cc3e0aba123f3a62e34
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-08-01 14:36:17 +0530

    Update dashboard.py

--- utils/dashboard.py
+++ utils/dashboard.py
@@ -1,1 +1,21 @@
+from database import cursor
 
+def total_records():
+    cursor.execute("SELECT COUNT(*) FROM history")
+    return cursor.fetchone()[0]
+
+def total_scans():
+    cursor.execute("SELECT COUNT(*) FROM history WHERE type='Medicine Scan'")
+    return cursor.fetchone()[0]
+
+def total_searches():
+    cursor.execute("SELECT COUNT(*) FROM history WHERE type='Medicine Search'")
+    return cursor.fetchone()[0]
+
+def total_interactions():
+    cursor.execute("SELECT COUNT(*) FROM history WHERE type='Drug Interaction'")
+    return cursor.fetchone()[0]
+
+def total_chats():
+    cursor.execute("SELECT COUNT(*) FROM history WHERE type='AI Chat'")
+    return cursor.fetchone()[0]
