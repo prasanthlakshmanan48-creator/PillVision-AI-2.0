e55372426a7ab1ca664a3dd23baace0205ab6f04
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-08-01 14:07:55 +0530

    Update history.py

--- utils/history.py
+++ utils/history.py
@@ -1,39 +1,53 @@
-import streamlit as st
 from datetime import datetime
+from database import conn, cursor
 
-# Add new history record
+
 def add_history(history_type, title, content):
 
-    if "history" not in st.session_state:
-        st.session_state.history = []
+    cursor.execute(
+        """
+        INSERT INTO history(type,title,content,time)
+        VALUES(?,?,?,?)
+        """,
+        (
+            history_type,
+            title,
+            content,
+            datetime.now().strftime("%d-%m-%Y %H:%M")
+        )
+    )
 
-    st.session_state.history.insert(0, {
-        "type": history_type,
-        "title": title,
-        "content": content,
-        "time": datetime.now().strftime("%d-%m-%Y %H:%M")
-    })
+    conn.commit()
 
 
-# Get all history
 def get_history():
 
-    if "history" not in st.session_state:
-        st.session_state.history = []
+    cursor.execute(
+        """
+        SELECT type,title,content,time
+        FROM history
+        ORDER BY id DESC
+        """
+    )
 
-    return st.session_state.history
+    rows = cursor.fetchall()
+
+    history=[]
+
+    for row in rows:
+
+        history.append({
+            "type":row[0],
+            "title":row[1],
+            "content":row[2],
+            "time":row[3]
+        })
+
+    return history
 
 
-# Clear history
 def clear_history():
 
-    st.session_state.history = []
+    cursor.execute("DELETE FROM history")
 
-
-# Total records
-def history_count():
-
-    if "history" not in st.session_state:
-        return 0
-
-    return len(st.session_state.history)
+    conn.commit()
