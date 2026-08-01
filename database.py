1e752fd27e6acbc85889d82942955f3e4324b148
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-08-01 14:07:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,16 @@
+import sqlite3
 
+conn = sqlite3.connect("pillvision.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS history(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    title TEXT,
+    content TEXT,
+    time TEXT
+)
+""")
+
+conn.commit()
